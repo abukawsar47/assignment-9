56c6de60a8e83a50efbf31027ee26c8d27c084be
abukawsar47 <abukawsar47ak@gmail.com>
2022-04-04 23:55:22 +0600

    clear about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import About from './components/About/About';
 import Blogs from './components/Blogs/Blogs';
 import Dashboard from './components/Dashboard/Dashboard';
 import Home from './components/Home/Home';
@@ -18,7 +17,6 @@ function App() {
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
